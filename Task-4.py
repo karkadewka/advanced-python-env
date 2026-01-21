cee8bfc5caffaca36cbc843d39c89664a50b7d9d
karkadewka <142025884+karkadewka@users.noreply.github.com>
2026-01-21 14:16:33 +0500

    м

--- Task-4.py
+++ Task-4.py
@@ -1,22 +1,33 @@
 class Employee:
     def __init__(self, name, salary):
         self.name = name
-        self.salary = salary
+        self._salary = salary  # private attribute
 
-    def role(self):
+    def get_salary(self):
+        return self._salary
+
+    def get_role(self):
         return "Employee"
 
 
 class Manager(Employee):
     def __init__(self, name, salary):
         super().__init__(name, salary)
-        self.bonus = 0.1
+        self._bonus = 0.1
 
-    def role(self):
+    def get_role(self):
         return "Manager"
 
+    def get_bonus(self):
+        return self._salary * self._bonus
+
     def get_salary(self):
-        return self.salary + self.salary * self.bonus
+        return self._salary + self.get_bonus()
+
+
+def print_employees(employees):
+    for e in employees:
+        print(f"Name: {e.name} | Role: {e.get_role()} | Salary: {e.get_salary()} $")
 
 
 employees = [
@@ -24,8 +35,5 @@ employees = [
     Manager("Amina", 100000)
 ]
 
-for e in employees:
-    if e.role() == "Manager":
-        print(f"Name: {e.name} | Role: {e.role()} | Salary: {e.get_salary()} $")
-    else:
-        print(f"Name: {e.name} | Role: {e.role()} | Salary: {e.salary} $")
+print_employees(employees)
+
